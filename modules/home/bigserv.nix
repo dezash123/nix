d9757d131d4cf2e643135d17d02bf132e7a5ac42
dezash123 <desmondmehta@gmail.com>
2025-07-18 12:53:32 -0400

    bigserv packages

--- modules/home/bigserv.nix
+++ modules/home/bigserv.nix
@@ -1,4 +1,4 @@
-{inputs, username, host, ...}: {
+{inputs, username, host, pkgs, ...}: {
   imports = [
     ./bat.nix
     ./btop.nix
@@ -6,8 +6,48 @@
     ./fastfetch.nix
     ./git.nix
     ./nixvim
-    ./bigserv-packages.nix
     ./starship.nix
     ./zsh.nix
+  ];
+
+  home.packages = with pkgs; [
+    bitwise                           # cli tool for bit / hex manipulation
+    bottom
+    cargo-generate
+    conda
+    lsd                               # ls replacement
+    fastfetch                 # better neofetch
+    fd                                # find replacement
+    file                              # Show file information 
+    fzf                               # fuzzy finder
+    gtt                               # google translate TUI
+    gh
+    gtrash                            # rm replacement, put deleted files in system trash
+    hexdump
+    imagemagick
+    jdk17                             # java
+    lazygit
+    nitch                             # systhem fetch util
+    niv
+    nix-prefetch-github
+    openconnect
+    openssl
+    pipes                             # terminal screensaver
+    pkg-config
+    ripgrep                           # grep replacement
+    todo                              # cli todo list
+    valgrind                          # c memory analyzer
+    vim
+    yazi                              # terminal file manager
+
+    # C / C++
+    gcc
+    gnumake
+
+    rustup
+
+    openssl
+    unzip
+    wget
   ];
 }
