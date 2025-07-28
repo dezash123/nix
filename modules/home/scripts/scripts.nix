bbaa893db4f4ece451ac8ac2f1dedef900628bd5
dezash123 <desmondmehta@gmail.com>
2025-07-27 22:31:18 -0400

    remove unused scripts

--- modules/home/scripts/scripts.nix
+++ modules/home/scripts/scripts.nix
@@ -4,13 +4,9 @@
   
   runbg = pkgs.writeShellScriptBin "runbg" (builtins.readFile ./scripts/runbg.sh);
   music = pkgs.writeShellScriptBin "music" (builtins.readFile ./scripts/music.sh);
-  lofi = pkgs.writeScriptBin "lofi" (builtins.readFile ./scripts/lofi.sh);
   
   toggle_blur = pkgs.writeScriptBin "toggle_blur" (builtins.readFile ./scripts/toggle_blur.sh);
   toggle_oppacity = pkgs.writeScriptBin "toggle_oppacity" (builtins.readFile ./scripts/toggle_oppacity.sh);
-  
-  compress = pkgs.writeScriptBin "compress" (builtins.readFile ./scripts/compress.sh);
-  extract = pkgs.writeScriptBin "extract" (builtins.readFile ./scripts/extract.sh);
   
   shutdown-script = pkgs.writeScriptBin "shutdown-script" (builtins.readFile ./scripts/shutdown-script.sh);
   
@@ -18,8 +14,6 @@
   
   vm-start = pkgs.writeScriptBin "vm-start" (builtins.readFile ./scripts/vm-start.sh);
 
-  ascii = pkgs.writeScriptBin "ascii" (builtins.readFile ./scripts/ascii.sh);
-  
   record = pkgs.writeScriptBin "record" (builtins.readFile ./scripts/record.sh);
 in {
   home.packages = with pkgs; [
