8c2e9896e7f7b2353883dae921c2f28173fec4f6
dezash123 <desmondmehta@gmail.com>
2026-06-16 18:12:45 -0400

    update

--- modules/system/network/nix-top.nix
+++ modules/system/network/nix-top.nix
@@ -1,5 +1,23 @@
 {
-  services.tailscale.useRoutingFeatures = "client";
+  services = {
+    tailscale.useRoutingFeatures = "client";
+
+    openssh = {
+      enable = true;
+      openFirewall = false;
+      settings = {
+        PasswordAuthentication = true;
+        PermitRootLogin = "no";
+        X11Forwarding = false;
+        PrintMotd = false;
+        TCPKeepAlive = true;
+        ClientAliveInterval = 60;
+        ClientAliveCountMax = 3;
+      };
+    };
+  };
+
+  networking.firewall.interfaces.tailscale0.allowedTCPPorts = [ 22 ];
 
   hardware.bluetooth = {
     enable = true;
